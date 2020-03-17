1abdc6c73c39f39e8d22ad7ce478a6a42de35f6a
humuhimi <masa19951009@gmail.com>
2020-03-17 16:04:39 +0800

    overflow to visible ,backgroundcolor to black

---
+++ Django_samp_project/star_social_demo/static/star_social_demo/js/group_detail.js
@@ -0,0 +1,6 @@
+// $(function() {
+    document.body.style.backgroundColor="black"
+    document.body.style.backgroundImage = "url()"
+    document.body.style.overflow ="visible"
+    console.log("group_detail js done!")
+// }) 
